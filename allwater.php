394ef882c12d838ebbb658d4b61f6261a5e92b62
VSkor748 <v.skorobog@yandex.ru>
2023-04-30 11:31:41 +0300

    init commit

---
+++ allwater.php
@@ -0,0 +1,185 @@
+<?php 
+function funText($string)
+{
+    $arrStr = str_split($string, 1);
+    foreach ($arrStr as $let) {
+        echo '<span>'.$let.'</span>';
+    }
+}
+ 
+function watCreate($im)
+{
+    $pathImg = $im;
+    $pathCopy = preg_replace('#(.(?i)(jpg|jpeg|png))$#', 'copy$1', $im);
+    $pic = explode('waterMark/', $im);
+
+
+    if (file_exists($pathCopy)) {
+        funText($im);
+        echo ' -  <span style="color:tomato; font-size: 1.2em">уже был добавлен!</span><br>';
+        echo '<img src="' . $pic[1] . '" height="500" alt="Результат"><br><br>';
+
+        return;
+    }
+
+    // $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo-hitec.png');
+    // $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo-hitec-min.png');
+    // $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo-hitec-sm_text.png');
+    // $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo_hitek-min_opacity.png');
+    $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo.png');
+    $sx = imagesx($stamp);
+    $sy = imagesy($stamp);
+
+    switch (exif_imagetype($im)) {
+        case 3:
+            $img = imagecreatefrompng($pathImg);
+            $iy = imagesy($img);
+            if ($iy < 300) {
+                return;
+            }
+            $ix = imagesx($img);
+            imagepng($img, $pathCopy, 8);
+            imagecopy($img, $stamp, $ix / 2 - $sx / 2, $iy / 2 - $sy / 2, 0, 0, $sx, $sy);
+            imagepng($img, $pathImg, 8);
+            break;
+        case 2:
+            $img = imagecreatefromjpeg($pathImg);
+            $ix = imagesx($img);
+            $iy = imagesy($img);
+            imagejpeg($img, $pathCopy, 80);
+            imagecopy($img, $stamp, $ix / 2 - $sx / 2, $iy / 2 - $sy / 2, 0, 0, $sx, $sy);
+            imagejpeg($img, $pathImg, 80);
+            break;
+    }
+
+    imagedestroy($stamp);
+    imagedestroy($img);
+    funText($im);
+    echo ' - <span style="color:LawnGreen; font-size: 1.2em">успешно добавлен!</span><br>';
+    echo '<img src="' . $pic[1] . '" height="500" alt="Результат"><br><br>';
+}
+
+function watUpdate($im)
+{
+    $pathImg = $im;
+    $pathCopy = preg_replace('#(.(?i)(jpg|jpeg|png))$#', 'copy$1', $im);
+    $pic = explode('waterMark/', $im);
+
+
+    if (!file_exists($pathCopy)) {
+        watCreate($im);
+        return;
+    }
+
+    $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo-hitec.png');
+    // $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo-hitec-min.png');
+    // $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo-hitec-sm_text.png');
+    // $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo_hitek-min_opacity.png');
+    // $stamp = imagecreatefrompng($_SERVER['DOCUMENT_ROOT'] . '/logo/logo.png');
+    $sx = imagesx($stamp);
+    $sy = imagesy($stamp);
+
+    switch (exif_imagetype($im)) {
+        case 3:
+            $img = imagecreatefrompng($pathCopy);
+            $iy = imagesy($img);
+            if ($iy < 300) {
+                return;
+            }
+            $ix = imagesx($img);
+            imagecopy($img, $stamp, $ix / 2 - $sx / 2, $iy / 2 - $sy / 2, 0, 0, $sx, $sy);
+            imagepng($img, $pathImg, 8);
+            break;
+        case 2:
+            $img = imagecreatefromjpeg($pathCopy);
+            $ix = imagesx($img);
+            $iy = imagesy($img);
+            imagecopy($img, $stamp, $ix / 2 - $sx / 2, $iy / 2 - $sy / 2, 0, 0, $sx, $sy);
+            imagejpeg($img, $pathImg, 80);
+            break;
+    }
+
+    imagedestroy($stamp);
+    imagedestroy($img);
+    funText($im);
+    echo ' - <span style="color:LawnGreen; font-size: 1.2em">успешно добавлен!</span><br>';
+    echo '<img src="' . $pic[1] . '" height="500" alt="Результат"><br><br>';
+}
+
+function watRemove($im)
+{
+    // $pathImg = $_SERVER['DOCUMENT_ROOT'].'/'.$im;
+    $pathImg = $im;
+    $pathCopy = preg_replace('#(.(?i)(jpg|jpeg|png))$#', 'copy$1', $im);
+    $pic = explode('waterMark/', $im);
+
+    if (!file_exists($pathCopy)) {
+        funText($im);
+        echo ' -  <span style="color:tomato; font-size: 1.2em">уже отсутствовал!</span><br>';
+        echo '<img src="' . $pic[1] . '" height="500" alt="Результат"><br><br>';
+
+        return;
+    }
+
+    switch (exif_imagetype($im)) {
+        case 3:
+            $img = imagecreatefrompng($pathCopy);
+            imagepng($img, $pathImg, 8);
+            break;
+        case 2:
+            $img = imagecreatefromjpeg($pathCopy);
+            imagejpeg($img, $pathImg, 80);
+            break;
+    }
+
+    imagedestroy($img);
+    unlink($pathCopy);
+    funText($im);
+    echo ' - <span style="color:LawnGreen; font-size: 1.2em">успешно удален!</span><br>';
+    echo '<img src="' . $pic[1] . '" height="500" alt="Результат"><br><br>';
+}
+
+function prbr($it)
+{
+    echo '<pre>';
+    echo '<br>';
+    print_r($it);
+}
+
+function findFil($act, $fil)
+{
+    $dir = $_SERVER['DOCUMENT_ROOT'] . '/upload';
+
+    function sDir($dir, $act, $fil)
+    {
+        $files = array_diff(scandir($dir), ['..', '.']);
+        foreach ($files as $val) {
+            $path = $dir . '/' . $val;
+            if (is_file($path)) {
+                if (!preg_match('#(copy.(?i)(jpg|jpeg|png))$#', $path) && preg_match('#(.(?i)(jpg|jpeg|png))$#', $path) && preg_match('#' . $fil . '(.(?i)(jpg|jpeg|png))$#', $path)) {
+                    $pic = explode('waterMark/', $path);
+                    if ($act == 'create') {
+                        watCreate($path);
+                    } elseif ($act == 'remove') {
+                        watRemove($path);
+                    } elseif ($act == 'update') {
+                        watUpdate($path);
+                    } else {
+                        funText($path);
+                        echo '<br><img src="' . $pic[1] . '" height="500" alt="Результат"><br><br>';
+                    }
+                }
+            } elseif (is_dir($path)) {
+                sDir($path, $act, $fil);
+            }
+        }
+    }
+
+    sDir($dir, $act, $fil);
+}
+
+$action = $_GET['action'] ? $_GET['action'] : '';
+$file = $_GET['file'] ? $_GET['file'] : '';
+
+findFil($action, $file); 
+
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>waterMark</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body> 
+    <div class="block">
+        <h2><span>Н</span><span>а</span><span>л</span><span>е</span><span>п</span><span>и</span><span>т</span><span>ь</span> <span>л</span><span>э</span><span>й</span><span>б</span><span>л</span> <span>в</span><span>с</span><span>е</span><span>м</span> <span> (</span><span>j</span><span>p</span><span>g</span><span>,</span><span> j</span><span>p</span><span>e</span><span>g</span><span>,</span><span> p</span><span>n</span><span>g</span><span>)</span><span>,</span><span> е</span><span>с</span><span>л</span><span>и</span> <span>н</span><span>е</span> <span>у</span><span>к</span><span>а</span><span>з</span><span>а</span><span>н</span><span>о</span> <span>И</span><span>м</span><span>я</span><span></h2>
+        <form action="/" method="get">
+            <label>
+                File Name
+                <input class="input_name"  type="text" name="file" placeholder="without file extension" value="<?= $_GET['file'] ?>">
+            </label>
+            <br>
+            <label class="radio_label">
+                Create
+                <input type="radio" name="action" value="create">
+            </label>
+            <label class="radio_label">
+                Update
+                <input type="radio" name="action" value="update">
+            </label>
+            </label>
+            <label class="radio_label">
+                Delete
+                <input type="radio" name="action" value="remove">
+            </label><br>
+            <button>Выполнить</button>
+        </form>
+    </div>
+ 
+    <div class="block 111">
+        <p>
+            <?php  
+                include './allwater.php'; 
+            ?>
+        </p>
+    </div>
+
+    <div class="block 222">
+        <p> <?php funText('Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quam nam fugiat error nulla. Deleniti, animi! Perferendis praesentium unde dolor sunt nihil nisi eveniet fugiat dolore aspernatur inventore autem quis, voluptatibus laudantium obcaecati magnam minus doloremque quas. Laudantium obcaecati tenetur blanditiis neque cum. Exercitationem, eveniet! Vel numquam, eligendi asperiores libero illum iure ducimus explicabo soluta unde suscipit. Id corporis ullam deleniti aliquid laudantium quo omnis, ad quaerat, culpa similique quos officiis nulla tempora pariatur. Itaque laboriosam corrupti ea eius ducimus quae autem ut voluptas sequi nihil, quisquam minima aliquam inventore officia tempore rerum veniam maxime nam odio provident? Commodi delectus minima nulla non? Voluptatem, ipsa cumque. Deleniti cum culpa iste pariatur suscipit excepturi saepe ipsum, commodi corporis doloremque delectus exercitationem eveniet maxime voluptate recusandae optio earum, laudantium voluptates animi dolores fugiat, ex magnam reiciendis. Voluptatibus illo maxime aspernatur eum molestias corporis pariatur repellendus tempora accusamus praesentium ratione excepturi animi alias consequuntur accusantium perferendis ad porro deserunt, odit doloremque quas nulla placeat. Deserunt excepturi nulla consequuntur ipsum est iusto et vitae dolorum voluptate, ratione debitis molestias distinctio quo. Recusandae eius totam repellat incidunt laudantium laboriosam, provident maiores, nihil natus quisquam laborum reprehenderit, error impedit. In accusantium iusto impedit, illo nobis suscipit, maxime exercitationem voluptates veritatis alias officiis quidem, illum cumque. Officia nostrum maiores minima deserunt quidem in, magni earum, eveniet saepe ducimus, unde doloremque sequi fugit iste eos beatae voluptate. Error nesciunt sapiente dolorem doloribus, eligendi ipsum, voluptatibus ad cum maxime optio enim iusto voluptate vel soluta placeat cupiditate, autem atque facere? Laboriosam quo nesciunt odio! Ex exercitationem rem ullam eaque, doloribus autem. Cupiditate omnis illum,  eligendi asperiores libero illum iure ducimus explicabo soluta unde suscipit. Id corporis ullam deleniti aliquid laudantium quo omnis, ad quaerat, culpa similique quos officiis nulla tempora pariatur. Itaque laboriosam corrupti ea eius ducimus quae autem ut voluptas sequi nihil, quisquam minima aliquam inventore officia tempore rerum veniam maxime nam odio provident? Commodi delectus minima nulla non? Voluptatem, ipsa cumque. Deleniti cum culpa iste pariatur suscipit excepturi saepe ipsum, commodi corporis doloremque delectus exercitationem eveniet maxime voluptate recusandae optio earum, laudantium voluptates animi dolores fugiat, ex magnam reiciendis. Voluptatibus illo maxime aspernatur eum molestias corporis pariatur repellendus tempora accusamus praesentium ratione excepturi animi alias consequuntur accusantium perferendis ad porro deserunt, odit doloremque quas nulla placeat. Deserunt excepturi nulla consequuntur ipsum est iusto et vitae dolorum voluptate, ratione debitis molestias distinctio quo. Recusandae eius totam repellat incidunt laudantium laboriosam, provident maiores, nihil natus quisquam laborum reprehenderit, error impedit. In accusantium iusto impedit, illo nobis suscipit, maxime exercitationem nisi sed commodi doloremque molestias numquam quas repellendus officia eum!')?></p>
+    </div>
+ 
+</body>
+
+</html>
